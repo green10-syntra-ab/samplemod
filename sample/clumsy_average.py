5aac3ce0f380789614c84e3d6d4dcfb2e7d3e7cc
hansveb <hans.vandenbogaerde@gmail.com>
2019-10-01 16:13:02 +0200

    Revert "initial commit"
    This reverts commit 67a30b7c

--- sample/clumsy_average.py
+++
@@ -1,9 +0,0 @@
-def calculate_average(*args):
-    sum = 0.0
-    for arg in args:
-        sum += arg
-    return sum/3.0
-
-
-assert 2.0 == calculate_average(1, 2, 3)
-assert 3.0 == calculate_average(1, 2, 3, 6)
